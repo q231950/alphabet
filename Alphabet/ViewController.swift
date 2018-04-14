950124a8acb190b1c92dfce925b89d0435e21efa
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2018-04-14 13:13:37 +0200

    Initial Commit

---
+++ Alphabet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alphabet
+//
+//  Created by Kim Dung-Pham on 14.04.18.
+//  Copyright © 2018 elbedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
